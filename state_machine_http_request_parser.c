3fe723e9ae2cbc15973b18a8ee0eaafbf619afef
Yuan <49439486+yuanluo2@users.noreply.github.com>
2024-04-02 13:46:32 +0800

    Update state_machine_http_request_parser.c

--- state_machine_http_request_parser.c
+++ state_machine_http_request_parser.c
@@ -174,10 +174,10 @@ int hex_to_decimal(char c) {
     if (c >= '0' && c <= '9'){
         return c - '0';
     }
-    else if (c >= 'a' && c <= 'z'){
+    else if (c >= 'a' && c <= 'f'){
         return c - 'a' + 10;
     }
-    else if (c >= 'A' && c <= 'Z'){
+    else if (c >= 'A' && c <= 'F'){
         return c - 'A' + 10;
     }
 
